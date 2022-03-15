ae8665c74ebb4549ed0056187205932adceb631a
Leonardo Amorim <74081681+Leow14@users.noreply.github.com>
2022-03-15 18:00:02 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,10 @@
+var num1 = prompt("Digite o primeiro número");
+var num2 = prompt("Digite o segundo número");
+
+if (num1 < num2){
+    document.write(num2);
+}
+
+else{
+    document.write(num1);
+}
---
+++ teste1.js
@@ -0,0 +1,17 @@
+var hora = 42;
+
+if (hora > 12) {
+    alert("Bom dia!")
+}
+
+if (hora < 12, hora < 17){
+    alert("Boa tarde!")
+}
+
+if (hora < 17) {
+    alert("Boa noite!")
+}
+
+else{
+    alert("Nem sei que horas são")
+}
